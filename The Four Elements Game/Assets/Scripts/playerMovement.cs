5389da0f3257617f6b217d43782a8b029bdea468
DanCreation <danjmorris21@gmail.com>
2022-04-12 18:07:20 +0100

    Fixed Jumping and added collectibles and a new font
    Fixed the jumping so you can't spam the jump button and fly away. Added a collectible called cherries to the game and added a new font that counts the cherries as you collect them in the top left hand corner of the screen.

--- The Four Elements Game/Assets/Scripts/playerMovement.cs
+++ The Four Elements Game/Assets/Scripts/playerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
@@ -18,6 +21,7 @@ public class playerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -28,7 +32,7 @@ public class playerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -65,5 +69,10 @@ public class playerMovement : MonoBehaviour
         }
 
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
